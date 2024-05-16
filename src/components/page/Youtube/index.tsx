9fadcd4d12e6d97a727ade70ed8e85c93d8705cb
lt_이인택 <hotdog4750323@naver.com>
2024-05-16 20:51:09 +0900

    영상 업로드 전 미리보기 추가

--- src/components/page/Youtube/index.tsx
+++ src/components/page/Youtube/index.tsx
@@ -1,10 +1,10 @@
 import {getVideoList} from "@/client/video";
-import {useEffect, useState} from "react";
-import {Alert, Skeleton} from "antd";
+import {useCallback, useEffect, useState} from "react";
+import {Alert, Select, Skeleton} from "antd";
 import styled from "styled-components";
-import {Plus} from "lucide-react";
 import ShortsLogo from 'public/img/logo/Youtube_shorts_icon.svg'
 import Image from "next/image";
+import DefaultModal from "@/components/shared/ui/default-modal";
 
 const UploadYoutubeWrapper = styled.div`
   width: 315px;
@@ -32,7 +32,9 @@ export const YoutubeUpload = () => {
   const [notUploadVideoList, setNotUploadVideoList] = useState<any[]>([])
   const [errorMessage, setErrorMessage] = useState<string>('')
   const [isError, setIsError] = useState(false)
-
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [updateLoading, setUpdateLoading] = useState(false);
+  const [selectUploadVideo, setSelectUploadVideo] = useState<number | undefined>(undefined);
   const errorHandle = (e: any) => {
     setErrorMessage(e?.message || '알수 없는 에러');
     setIsError(true)
@@ -46,6 +48,17 @@ export const YoutubeUpload = () => {
         isDeleted: string
       }) => !!uploadId && isDeleted !== 'Y');
       setUploadVideoList(uploadList);
+      const notUploadList = response.data.filter(({uploadId, isDeleted}: {
+        uploadId: string,
+        isDeleted: string
+      }) => !uploadId && isDeleted !== 'Y').map((video: any) => {
+        return {
+          value: video.id,
+          label: `${video.gptTitle} (${video.title || '제목 없음'})`,
+          disabled: !video.title || !video.content,
+        }
+      });
+      setNotUploadVideoList(notUploadList)
       setIsLoading(false)
     } catch (e) {
       errorHandle(e)
@@ -53,6 +66,13 @@ export const YoutubeUpload = () => {
     }
 
   }
+
+  const UploadVideoComponent = useCallback(() => {
+    return (<video controls>
+      <source src={`${process.env.NEXT_PUBLIC_API_ENDPOINT}/api/video/preview/${selectUploadVideo}`}
+              type={'video/mp4'}/>
+    </video>)
+  }, [selectUploadVideo])
 
   useEffect(() => {
     getVideoListData()
@@ -63,6 +83,21 @@ export const YoutubeUpload = () => {
 
   return (
     <>
+      <DefaultModal
+        handleHide={() => {
+          setIsModalOpen(false)
+        }}
+        title={`영상 업로드`}
+        open={isModalOpen}
+        confirmLoading={updateLoading}
+        onOk={() => {
+          setUpdateLoading(false)
+        }}>
+        <Select options={notUploadVideoList} className={'w-full'} onSelect={(data) => {
+          setSelectUploadVideo(data);
+        }}/>
+        {selectUploadVideo && (<UploadVideoComponent/>)}
+      </DefaultModal>
       {isError && <Alert
         className={'mb-5'}
         message={errorMessage}
@@ -77,25 +112,30 @@ export const YoutubeUpload = () => {
         <UploadYoutubeWrapper className={'rounded-md'}>
           <UploadTitle className={'m-3'}>영상 업로드</UploadTitle>
           <UploadIcon>
-            <Image src={ShortsLogo} alt={'shorts'} width={68}/>
+            <Image src={ShortsLogo} alt={'shorts'} width={68} onClick={() =>
+              setIsModalOpen(true)
+            }/>
           </UploadIcon>
         </UploadYoutubeWrapper>
-        {isLoading ? <Skeleton.Image active={true} style={{width: '315px', height:'560px'}}/> : uploadVideoList.map(({uploadId}: { uploadId: string }) => {
-          return (
-            <iframe
-              key={uploadId}
-              className={'rounded-md'}
-              width="315"
-              height="560"
-              src={`https://www.youtube.com/embed/${uploadId}`}
-              title="YouTube video player"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media;
+        {isLoading ?
+          <Skeleton.Image active={true} style={{width: '315px', height: '560px'}}/> : uploadVideoList.map(({uploadId}: {
+            uploadId: string
+          }) => {
+            return (
+              <iframe
+                key={uploadId}
+                className={'rounded-md'}
+                width="315"
+                height="560"
+                src={`https://www.youtube.com/embed/${uploadId}`}
+                title="YouTube video player"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media;
 gyroscope; picture-in-picture;
 web-share"
-              allowFullScreen
-            />
-          )
-        })}
+                allowFullScreen
+              />
+            )
+          })}
       </div>
     </>
   )
